52d429023b8ffb679ce2fc66c26f25393592ffa9
Sipann <vauguste92@gmail.com>
2020-05-17 16:32:07 +0200

    refactor: simplify component logic

--- CardTwoZones/card-two-zones.js
+++ CardTwoZones/card-two-zones.js
@@ -14,20 +14,56 @@ templateCard.innerHTML = `
 
 section {
   perspective: 1000px;
-}
-
-section .card {
   width: 100%;
   height: 100%;
-  display: grid;
-  grid-template-rows: var(--top-height) var(--bottom-height);
-  border-radius: var(--rounded);
-  box-shadow: var(--shadow, 5px) var(--shadow, 5px) var(--shadow, 5px) var(--shadow-color, #bbb);
+}
 
+.card {
+  width: 100%;
+  height: 100%;
   position: relative;
+  border-radius: var(--rounded);
   transform-style: preserve-3d;
-  backface-visibility: hidden;
-  transition: transform 1s;
+  transition: all var(--flip-duration, 0.8s) linear;
+}
+
+.flipped .card {
+  animation-name: flip-card;
+  animation-delay: 0;
+  animation-duration: var(--flip-duration, 0.8s);
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+}
+
+@keyframes flip-card {
+  50% { transform: rotateY(90deg); }
+  100% { transform: rotateY(180deg); }
+}
+
+.shadow {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: var(--shadow-color, rgba(0, 0, 0, 0.5));
+  border-radius: var(--rounded);
+  filter: blur(5px);
+  transform: scale3d(1, 1, 1) translate3d(5px, 10px, 0);
+}
+
+.flipped .shadow {
+  animation-name: reverse-shadow;
+  animation-delay: 0;
+  animation-duration: var(--flip-duration, 0.8s);
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+  transform: rotateY(0) translate3d(5px 10px, 0) scale3d(1, 1, 1);
+}
+
+@keyframes reverse-shadow {
+  50% { transform: rotateY(90deg) translate3d(0, 0, 0) scale3d(1, 0.8, 1); }
+  100% { transform: rotateY(180deg) translate3d(-10px, 10px, 0) scale3d(1,1,1);}
 }
 
 .front,
@@ -35,35 +71,44 @@ section .card {
   position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100%;
   backface-visibility: hidden;
+  display: grid;
+  border-radius: var(--rounded);
+}
+
+.front {
+  grid-template-rows: var(--front-top-height) var(--front-bottom-height);
 }
 
 .back {
   transform: rotateY(180deg);
+  background-color: var(--bg-back, #eee);
+  border-radius: var(--rounded);
 }
 
-.flipped .card {
-  transform: rotateY(180deg);
-}
-
-.top {
+.front-top {
   position: relative;
-  background: var(--bg-top, #aba6bf);
+  background: var(--bg-front-top, #eee);
   border-radius: var(--rounded) var(--rounded) 0 0;
 }
 
 #top-slot::slotted(*) {
   height: 100%;
+  width: 100%;
+  border-radius: var(--rounded) var(--rounded) 0 0;
 }
 
-.bottom {
-  background: var(--bg-bottom, #eee);
+.front-bottom {
+  background: var(--bg-front-bottom, #eee);
   border-radius: 0 0 var(--rounded) var(--rounded);
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  color: var(--front-bottom-color, #333);
 }
 
 #content {
@@ -74,7 +119,7 @@ section .card {
   position: absolute;
   width: 40px;
   height: 40px;
-  background: var(--flip-bg, #595775);
+  background: var(--btn-flip-bg, #595775);
   top: -20px;
   right: 10%;
   border-radius: 50%;
@@ -85,22 +130,27 @@ section .card {
 }
 
 .flip path:nth-of-type(1) {
-  stroke: var(--flip-icon, #fff);
+  stroke: var(--flip-icon-fill, #fff);
 }
 
 .flip path:nth-of-type(2) {
-  fill: var(--flip-icon, #fff);
+  fill: var(--flip-icon-fill, #fff);
+}
+
+.back-content {
+  border-radius: var(--rounded);
 }
 
 </style>
 
 <section>
+  <div class="shadow"></div>
   <div class="card">
     <div class="front">
-      <div class="top">
-        <slot id="top-slot" name="top"></slot>
+      <div class="front-top">
+        <slot id="top-slot" name="front-top-slot"></slot>
       </div>
-      <div class="bottom">
+      <div class="front-bottom">
         <div class="flip">
           <slot name="icon">
             <svg viewBox="0 0 100 100">
@@ -109,15 +159,19 @@ section .card {
             </svg>
           </slot>
         </div>
-        <slot id="content-title" name="title"></slot>
-        <slot id="content-summary" name="summary"></slot>
+        <slot name="front-bottom-title"></slot>
+        <slot name="front-bottom-subtitle"></slot>
       </div>
     </div>
-    <div class="back">BACK</div>
+
+    <div class="back">
+      <div class="back-content">
+        <slot name="back-content"></slot>
+      </div>
+    </div>
   </div>
 </section>
 `;
-
 
 class CardTemplate extends HTMLElement {
 
@@ -126,48 +180,26 @@ class CardTemplate extends HTMLElement {
     this._shadowRoot = this.attachShadow({ mode: 'open' })
     this._shadowRoot.appendChild(templateCard.content.cloneNode(true))
 
-    this.$button = this.shadowRoot.querySelector('.flip')
     this.$container = this.shadowRoot.querySelector('section');
-
+    this.$button = this.shadowRoot.querySelector('.flip')
   }
 
   connectedCallback () {
-    let rounded = this.hasAttribute('rounded') ? 5 : 0
-    this.shadowRoot.host.style.setProperty('--rounded', `${rounded}%`)
+    const height = window.getComputedStyle(this).getPropertyValue('height').replace(/[a-z]/gi, '');
+    const top = height * this.getAttribute('top') || height * 2 / 3;
+    const bottom = height - top;
+    this.shadowRoot.host.style.setProperty('--front-top-height', `${top}px`);
+    this.shadowRoot.host.style.setProperty('--front-bottom-height', `${bottom}px`);
 
-    if (this.hasAttribute('flippable')) {
-      this.$button.addEventListener('click', this._flip.bind(this))
-    } else {
-      this.$button.style.display = 'none'
-    }
-
-    let top;
-    let bottom;
-    let height = window.getComputedStyle(this).getPropertyValue('height')
-    let regex = /[a-z]/gi
-    height = height.replace(regex, '')
-    if (this.hasAttribute('top')) {
-      top = this.getAttribute('top')
-    } else {
-      top = height * 2 / 3
-    }
-    bottom = height - top
-    this.shadowRoot.host.style.setProperty('--top-height', `${top}px`)
-    this.shadowRoot.host.style.setProperty('--bottom-height', `${bottom}px`)
-
-    this.addEventListener('flip', () => {
-      this.$container.classList.add('flipped');
-    });
+    this.$button.addEventListener('click', this._flip.bind(this))
   }
 
   disconnectedCallback () {
     this.$button.removeEventListener('click', this._flip)
-    this.removeEventListener('flip');
   }
 
   _flip () {
-    const flipEvent = new Event('flip')
-    this.dispatchEvent(flipEvent)
+    this.$container.classList.add('flipped');
   }
 
 }
